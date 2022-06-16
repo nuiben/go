0efd7e65466e1c47d45548770bc64b39366324b5
nuiben <bgporte@outlook.com>
2022-06-15 22:54:40 -0500

    Go Hash Table

---
+++ hashTable.go
@@ -0,0 +1,111 @@
+package main
+
+import "fmt"
+
+const ArraySize = 7
+
+// HashTable Structure
+type HashTable struct {
+	array [ArraySize]*bucket
+}
+
+// Insert
+func (h *HashTable) Insert(key string) {
+	index := hash(key)
+	h.array[index].insert(key)
+}
+
+// Search
+func (h *HashTable) Search(key string) bool {
+	index := hash(key)
+	h.array[index].insert(key)
+
+}
+
+// Delete
+func (h *HashTable) Delete(key string) bool {
+	index := hash(key)
+}
+
+// Bucket Structure (linked list)
+type bucket struct {
+	head *bucketNode
+}
+
+// bucketNode Structure
+type bucketNode struct {
+	key  string
+	next *bucketNode
+}
+
+// Insert
+func (b *bucket) insert(k string) {
+	if !b.search(k) {
+		newNode := &bucketNode{key: k}
+		newNode.next = b.head
+		b.head = newNode
+	} else {
+		fmt.Println(k, " already exists")
+	}
+}
+
+// Search
+func (b *bucket) search(k string) bool {
+	currentNode := b.head
+	for currentNode != nil {
+		if currentNode.key == k {
+			return true
+		}
+		currentNode = currentNode.next
+	}
+	return false
+}
+
+// Delete
+func (b *bucket) delete(k string) {
+
+	if b.head.key == k {
+		b.head = b.head.next
+		return
+	}
+
+	previousNode := b.head
+	for previousNode.next != nil {
+		if previousNode.next.key == k {
+			previousNode.next = previousNode.next.next
+		}
+		previousNode = previousNode.next
+
+	}
+}
+
+// Hash
+func hash(key string) int {
+	sum := 0
+	for _, v := range key {
+		sum += int(v)
+	}
+	return sum % ArraySize
+}
+
+// Init
+func Init() *HashTable {
+	result := &HashTable{}
+	for i := range result.array {
+		result.array[i] = &bucket{}
+	}
+	return result
+}
+
+func main() {
+	// testHashTable := Init()
+	// fmt.Println(testHashTable)
+	// fmt.Println(hash("RANDY"))
+
+	testBucket := &bucket{}
+	testBucket.insert("RANDY")
+	testBucket.insert("RANDY")
+
+	fmt.Println(testBucket.search("RANDY"))
+	fmt.Println(testBucket.search("ERIC"))
+}
